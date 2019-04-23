990f8faa27db5d7a2b9297583ab3c063d53aa094
jpopxfile <raynold.sim@gmail.com>
2019-04-23 14:51:53 +0900

    Change notifications from array to map

--- src/notification.ts
+++ src/notification.ts
@@ -1,16 +1,29 @@
 import axios from 'axios'
 import { INotification } from './types'
 
-const notifications: INotification[] = []
+const notifications = new Map<string, INotification[]>()
 
-export const setNotification = (notification: INotification) =>
-  notifications.push(notification)
-export const getNotification = (topic: string) =>
-  notifications.filter(notification => notification.topic === topic)
+export const setNotification = (notificationObj: INotification) => {
+  const topic = notificationObj.topic
+  const notification = notifications.get(topic)
+  if (!notification) {
+    notifications.set(topic,[notificationObj])
+  } else {
+    notification.push(notificationObj)
+    notifications.set(topic,notification)
+  }
+}
+
+export const getNotification = (topic: string) :INotification[] => {
+  const notification = notifications.get(topic)
+  if (notification) {
+    return notification
+  }
+  return []
+}
 
 export const pushNotification = (topic: string) => {
   const notifications = getNotification(topic)
-
   if (notifications && notifications.length) {
     notifications.forEach((notification: INotification) =>
       axios.post(notification.webhook, { topic })
